4f4e2a7034317b0fab0176ffc6f6ea748115122c
Vict <84346021+Victimarius@users.noreply.github.com>
2021-05-22 22:00:48 -0400

    Added @Overwrite for MillVillager's toggleDoor in order to support BlockDoor's toggleDoor, used by mods such as Malisis

--- src/main/java/vict/millmix/mixin/MixinMillVillager.java
+++ src/main/java/vict/millmix/mixin/MixinMillVillager.java
@@ -15,6 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import zone.rong.mixinbooter.MixinLoader;
 
+import org.millenaire.common.utilities.Point;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockDoor;
+import  net.minecraft.block.Block ;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.util.math.BlockPos;
+
 @Mixin(MillVillager.class)
 public abstract class MixinMillVillager extends EntityCreature  {
 
@@ -27,5 +34,24 @@ public abstract class MixinMillVillager extends EntityCreature  {
         if (target != null && target instanceof EntityPlayer) {
             ci.cancel();
         } 
+    }
+    /* idea: change millvillager's toggleDoor to use BlockDoor's toggleDoor
+     * @author Vict
+     * @reason toggleDoor doesn't make use of BlockDoor.toggleDoor(),
+     * causing issues when other mods override the behaviour of doors.
+     * This way we don't manually set block states, but instead tell
+     * the blocks to change their staes.
+     */
+    @Overwrite(remap=false)
+    private void toggleDoor(Point p) {
+        if (p == null) {return;}
+        IBlockState state = p.getBlockActualState(this.world);
+        boolean isOpen = state.getValue((IProperty)BlockDoor.OPEN).equals(true);
+        BlockPos doorPos = p.getBlockPos();
+        Block block = p.getBlock(world);
+        if (block != null && block instanceof BlockDoor) {
+            BlockDoor door = (BlockDoor)block;
+            door.toggleDoor(world,doorPos, !isOpen);
+        }
     }
 }
